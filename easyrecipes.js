53a59f40b6b1fc6f0425ce497136eaf7d27235b9
Simone Ferrero <simone.ferrero@outlook.com>
2016-04-12 11:54:54 +0100

    Added responsiveness
    Added new layout for mobile and smaller screens
    Added differences between landscape and portrait
    Tweaked the interface a little and fixed some bugs

--- easyrecipes.js
+++ easyrecipes.js
@@ -1,59 +1,105 @@
+var mq = window.matchMedia("(min-width: 1150px)");
 var app = angular.module("DosageCalc", []);
 
-var recipe = [];
+var recipe = [],
+  choices = ["no unit", "g", "kg", "pound", "ounces", "ml", "l", "teaspoon", "tablespoon", "cup", "pint", "gallon"];
 var index = 0;
 
-app.directive('myEnter', function () {
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.myEnter);
-                });
-
-                event.preventDefault();
-            }
+app.directive('myEnter', function() {
+  return function(scope, element, attrs) {
+    element.bind("keydown keypress", function(event) {
+      if(event.which === 13) {
+        scope.$apply(function() {
+          scope.$eval(attrs.myEnter);
         });
-    };
+        event.preventDefault();
+      }
+    });
+  };
 });
 
 Array.prototype.getIndexOfObject = function(value){
-   for (var i = 0; i < this.length ; i++) {
-            if (this[i]["ii"] === value) {
-                return i;
-            }
-     }
+  for (var i = 0; i < this.length ; i++) {
+    if (this[i]["ii"] === value) {
+      return i;
+    }
+  }
 }
 
 app.controller("Ingredients", ["$scope", function($scope) {
   $scope.recipe = recipe;
   $scope.originalNumber = 1;
   $scope.newNumber = 1;
+  $scope.orPeople = "person";
+  $scope.newPeople = "person";
   $scope.ingredient = "";
   $scope.quantity = "";
-  $scope.choices = ["no unit", "g", "mg", "l", "ml"];
-  $scope.add = function() {
-    if (ingredient.value != "" && quantity.value != "") {
-      recipe.unshift({
-        quantity: quantity.value,
-        unit: units.value === "no unit" ? null : units.value,
-        ingredient: ingredient.value,
-        ii: index,
-        remove: function() {
-          var i = recipe.getIndexOfObject(this.ii);
-          recipe.splice(i, 1);
-          $("#quantity")
-            .focus();
-          console.log(recipe);
-        }
-      });
-      quantity.value = "";
-      ingredient.value = "";
-      index++;
-      $("#quantity")
-        .focus();
+  $scope.choices = choices;
+  $scope.personOrPeople = function() {
+    if ($scope.originalNumber == 1) {
+      $scope.orPeople = "person";
+    } else {
+      $scope.orPeople = "people";
     }
-    console.log(recipe);
-    console.log(index);
+    if ($scope.newNumber == 1) {
+      $scope.newPeople = "person";
+    } else {
+      $scope.newPeople = "people";
+    }
+  }
+  $scope.add = function() {
+    return newIngredient();
+  };
+  $scope.showMenu = function() {
+    return clickMenu();
+  };
+  $scope.showOriginal = function() {
+    return clickOriginal();
+  };
+  $scope.showNew = function() {
+    return clickNew();
   };
 }]);
+
+function newIngredient() {
+  if (ingredient.value != "" && quantity.value != "") {
+    recipe.unshift({
+      quantity: quantity.value,
+      unit: units.value === "no unit" ? null : units.value,
+      ingredient: ingredient.value,
+      ii: index,
+      remove: function() {
+        var i = recipe.getIndexOfObject(this.ii);
+        recipe.splice(i, 1);
+        $("#quantity")
+          .focus();
+      }
+    });
+    quantity.value = "";
+    ingredient.value = "";
+    index++;
+    $("#quantity")
+      .focus();
+  }
+};
+
+function clickMenu() {
+  $("#choose_recipe > h3")
+    .toggle(200);
+  $(".selector")
+    .toggle(200);
+}
+
+function clickOriginal() {
+  $("#original")
+    .show(200);
+  $("#new")
+    .hide(200);
+}
+
+function clickNew() {
+  $("#original")
+    .hide(200);
+  $("#new")
+    .show(200);
+}
